d99bd22a1cf0eb79344fb98cd0557967257c75fa
ogkuav <uchaev100@yandex.ru>
2021-04-12 10:28:29 +0300

    Работающая программа

--- Data/DBWork/DbOctagonalGasket.cs
+++ Data/DBWork/DbOctagonalGasket.cs
@@ -5,12 +5,13 @@ namespace StudCalculator.Data.DBWork
 {
     public class DbOctagonalGasket
     {
-        readonly DbModelFromVnmData.DbModelFromVnmData db = new();
+        readonly DbModelFromVnmData.DbModelFromVnmData _db = new();
 
         public double ExecutedOctogonalGasket(string pn, string dn)
         {
             var executedOctogonalGasket =
-                Convert.ToDouble(db.OGK_StudCalculator_Octagonal_Gasket.Where(p => p.PN == pn && p.DN == dn).Select(p => p.c).First());
+                Convert.ToDouble(_db.OGK_StudCalculator_Octagonal_Gasket.Where(p => p.PN == pn && p.DN == dn)
+                    .Select(p => p.c).First());
             return executedOctogonalGasket;
         }
     }
